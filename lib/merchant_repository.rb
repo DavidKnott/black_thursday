4830e3f43108bb4f81e3dac132bc422c9d7887bb
Laszlo Balogh <laszlo@thebaloghs.com>
2016-11-01 05:20:22 -0600

    All MerchantRepository methods and their tests

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -10,11 +10,32 @@ class MerchantRepository
     load_merchants(file_path)
   end
 
-
   def load_merchants(file_path)
     merchants_csv = CSV.open(file_path, headers:true, header_converters: :symbol)
     merchants_csv.each do |one_merchant|
       @merchants_list << Merchant.new({:id => one_merchant[:id], :name => one_merchant[:name]})
+    end
+  end
+
+  def all
+    merchants_list
+  end
+
+  def find_by_id(merchant_id)
+    merchants_list.find do |merchant|
+      merchant.id == merchant_id
+    end
+  end
+
+  def find_by_name(merchant_name)
+    merchants_list.find do |merchant|
+      merchant.name == merchant_name
+    end
+  end
+
+  def find_all_by_name(merchant_name)
+    merchants_list.find_all do |merchant|
+      merchant.name.include?(merchant_name)
     end
   end
 
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -5,10 +5,12 @@ require './lib/merchant_repository'
 
 class MerchantRepositoryTest < Minitest::Test
 
-  attr_reader    :test_merchant_repo
+  attr_reader   :test_merchant_repo,
+                :test_one_merchant_repo
 
   def setup
     @test_merchant_repo = MerchantRepository.new("./data/merchants.csv")
+    @test_one_merchant_repo = MerchantRepository.new("./data/merchants_one.csv")
   end
 
   def test_initialize_merchant_repository
@@ -26,4 +28,56 @@ class MerchantRepositoryTest < Minitest::Test
   def test_merchant_list_stores_id
     assert_equal "12334208", test_merchant_repo.merchants_list[26].id
   end
+
+  def test_all_method_on_csv_with_one_merchant
+    assert_equal 1, test_one_merchant_repo.all.count
+    assert_equal 475, test_merchant_repo.all.count
+  end
+
+  def test_all_method_on_merchant_all_details_id
+    assert_equal "12334165", test_one_merchant_repo.all.first.id
+    assert_equal "12334105", test_merchant_repo.all.first.id
+  end
+
+  def test_all_method_on_merchant_all_details_name
+    assert_equal "JUSTEmonsters", test_one_merchant_repo.all.first.name
+    assert_equal "Shopin1901", test_merchant_repo.all.first.name
+  end
+
+  def test_find_by_id_not_in_merchant_list
+    assert_nil test_one_merchant_repo.find_by_id("555")
+    assert_nil test_merchant_repo.find_by_id("999")
+  end
+
+  def test_find_by_id_is_in_merchant_list
+    assert_equal "12334165", test_one_merchant_repo.find_by_id("12334165").id
+    assert_equal "JUSTEmonsters", test_one_merchant_repo.find_by_id("12334165").name
+    assert_equal "12334105", test_merchant_repo.find_by_id("12334105").id
+    assert_equal "Shopin1901", test_merchant_repo.find_by_id("12334105").name
+  end
+
+  def test_find_by_name_not_in_merchant_list
+    assert_nil test_one_merchant_repo.find_by_name("555")
+    assert_nil test_merchant_repo.find_by_name("999")
+  end
+
+  def test_find_by_name_is_in_merchant_list
+    assert_equal "12334165", test_one_merchant_repo.find_by_name("JUSTEmonsters").id
+    assert_equal "JUSTEmonsters", test_one_merchant_repo.find_by_name("JUSTEmonsters").name
+    assert_equal "12334105", test_merchant_repo.find_by_name("Shopin1901").id
+    assert_equal "Shopin1901", test_merchant_repo.find_by_name("Shopin1901").name
+  end
+  
+  def test_find_all_by_name_not_in_merchant_list
+    assert_empty test_one_merchant_repo.find_all_by_name("555")
+    assert_empty test_merchant_repo.find_all_by_name("999")
+  end
+
+  def test_find_all_by_name_is_in_merchant_list
+    assert_equal "12334165", test_one_merchant_repo.find_all_by_name("nster").first.id
+    assert_equal "JUSTEmonsters", test_one_merchant_repo.find_all_by_name("TEmo").first.name
+    assert_equal "12335519", test_merchant_repo.find_all_by_name("pin").last.id
+    assert_equal "TIGHTpinch", test_merchant_repo.find_all_by_name("pin").last.name
+  end
+
 end
