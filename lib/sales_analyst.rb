5175c9b9f1ad9e6621a156739d40a13bfbdd23e6
David Knott <david1k1nott@gmail.com>
2016-11-01 18:36:53 -0600

    iteration 1 first 3 methods and tests

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class SalesAnalyst
 
   attr_reader   :sales_engine
@@ -6,5 +7,52 @@ class SalesAnalyst
     @sales_engine = sales_engine
   end
 
+  def merchants_list
+    sales_engine.merchants.merchants_list
+  end
+
+  def items_list
+    sales_engine.items.items_list
+  end
+
+  def average_items_per_merchant
+    total_items/total_merchants
+  end
+
+  def total_items
+    items_list.length.to_f
+  end
+
+  def total_merchants
+    merchants_list.length
+  end
+
+  def average_items_per_merchant_standard_deviation
+    sum = items_per_merchant_list.values.inject do |sum, items_for_one_merchant|
+      sum + (items_for_one_merchant - average_items_per_merchant)**2
+    end
+    sum = sum / merchants_list.length-1
+    Math.sqrt(sum)
+  end
+
+
+  def items_per_merchant_list
+    linked_list = {}
+    merchants_list.map do |merchant|
+      linked_list[merchant] = items_per_merchant(merchant.id)
+    end
+    linked_list
+  end
+
+  def items_per_merchant(merchant_id)
+    merchant = sales_engine.merchants.find_by_id(merchant_id)
+    merchant.items.length
+  end
+
+  def merchants_with_high_item_count
+    items_per_merchant_list.values.find_all do |items_for_one_merchant|
+      items_for_one_merchant > average_items_per_merchant + average_items_per_merchant_standard_deviation
+    end
+  end
 
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -19,4 +19,37 @@ class SalesAnalystTest < MiniTest::Test
     assert sales_analyst
   end
 
+  def test_it_returns_merchants_list
+    assert_equal 475, sales_analyst.merchants_list.length
+  end
+
+
+  def test_it_returns_items_list
+    assert_equal 1367, sales_analyst.items_list.length
+  end
+
+  def test_it_finds_average_items_per_merchant
+    assert_equal 2.877894736842105, sales_analyst.average_items_per_merchant
+  end
+
+  def test_it_finds_how_man_items_one_merchant_has
+    assert_equal 1, sales_analyst.items_per_merchant(12334174)
+  end
+
+  def test_it_makes_array_for_how_many_items_each_merchant_has
+    assert_equal 475, sales_analyst.items_per_merchant_list.count
+  end
+
+  def test_it_finds_average_items_per_merchant_standard_deviation
+    skip
+    assert_equal 3.099372766779748, sales_analyst.average_items_per_merchant_standard_deviation
+  end
+
+
+  def test_it_lists_merchants_with_item_count_over_one_standard_devation
+    skip
+    assert_equal 65, sales_analyst.merchants_with_high_item_count.count
+  end
+
+
 end
