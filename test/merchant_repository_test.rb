fe4f48de93dfa8dbde30ca8e0388bb9ff196012a
David Knott <david1k1nott@gmail.com>
2016-11-03 14:59:57 -0600

    refactors merchant repo test

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -5,12 +5,10 @@ require './lib/merchant_repository'
 
 class MerchantRepositoryTest < Minitest::Test
 
-  attr_reader   :test_merchant_repo,
-                :test_one_merchant_repo
+  attr_reader   :test_merchant_repo
 
   def setup
     @test_merchant_repo = MerchantRepository.new("./data/merchants.csv", "parent")
-    @test_one_merchant_repo = MerchantRepository.new("./data/merchants_one.csv", "parent")
   end
 
   def test_initialize_merchant_repository
@@ -21,62 +19,43 @@ class MerchantRepositoryTest < Minitest::Test
     refute_empty test_merchant_repo.merchants_list
   end
 
-  def test_merchant_list_stores_name
-    assert_equal "Candisart", test_merchant_repo.merchants_list[1].name
-  end
-
-  def test_merchant_list_stores_id
-    assert_equal 12334208, test_merchant_repo.merchants_list[26].id
+  def test_merchant_list_stores_merchants
+    assert_equal Merchant, test_merchant_repo.merchants_list.first.class
   end
 
   def test_all_method_on_csv_with_one_merchant
-    assert_equal 1, test_one_merchant_repo.all.count
     assert_equal 475, test_merchant_repo.all.count
   end
 
   def test_all_method_on_merchant_all_details_id
-    assert_equal 12334165, test_one_merchant_repo.all.first.id
     assert_equal 12334105, test_merchant_repo.all.first.id
   end
 
   def test_all_method_on_merchant_all_details_name
-    assert_equal "JUSTEmonsters", test_one_merchant_repo.all.first.name
     assert_equal "Shopin1901", test_merchant_repo.all.first.name
   end
 
   def test_find_by_id_not_in_merchant_list
-    assert_nil test_one_merchant_repo.find_by_id(555)
     assert_nil test_merchant_repo.find_by_id(999)
   end
 
   def test_find_by_id_is_in_merchant_list
-    assert_equal 12334165, test_one_merchant_repo.find_by_id(12334165).id
-    assert_equal "JUSTEmonsters", test_one_merchant_repo.find_by_id(12334165).name
     assert_equal 12334105, test_merchant_repo.find_by_id(12334105).id
-    assert_equal "Shopin1901", test_merchant_repo.find_by_id(12334105).name
   end
 
   def test_find_by_name_not_in_merchant_list
-    assert_nil test_one_merchant_repo.find_by_name("555")
     assert_nil test_merchant_repo.find_by_name("999")
   end
 
   def test_find_by_name_is_in_merchant_list
-    assert_equal 12334165, test_one_merchant_repo.find_by_name("JUSTEmonsters").id
-    assert_equal "JUSTEmonsters", test_one_merchant_repo.find_by_name("JUSTEmonsters").name
-    assert_equal 12334105, test_merchant_repo.find_by_name("Shopin1901").id
     assert_equal "Shopin1901", test_merchant_repo.find_by_name("Shopin1901").name
   end
   
   def test_find_all_by_name_not_in_merchant_list
-    assert_empty test_one_merchant_repo.find_all_by_name("555")
     assert_empty test_merchant_repo.find_all_by_name("999")
   end
 
   def test_find_all_by_name_is_in_merchant_list
-    assert_equal 12334165, test_one_merchant_repo.find_all_by_name("nster").first.id
-    assert_equal "JUSTEmonsters", test_one_merchant_repo.find_all_by_name("TEmo").first.name
-    assert_equal 12335602, test_merchant_repo.find_all_by_name("pin").last.id
     assert_equal "ShopAtPinkFlamingo", test_merchant_repo.find_all_by_name("pin").last.name
   end
 
