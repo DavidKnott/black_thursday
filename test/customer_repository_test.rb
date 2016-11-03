3ba687d8688a75765481837a2e2c047cce57cdc1
David Knott <david1k1nott@gmail.com>
2016-11-03 14:52:30 -0600

    refacting tests

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -21,24 +21,8 @@ class CustomerRepositoryTest < Minitest::Test
     refute_empty customers_list
   end
 
-  def test_transaction_list_stores_id
-    assert_equal 14, customers_list[13].id
-  end
-
-  def test_item_list_stores_first_name
-    assert_equal "Casimer", customers_list[13].first_name
-  end
-
-  def test_item_list_stores_last_name
-    assert_equal "Hettinger", customers_list[13].last_name
-  end
-
-  def test_item_list_stores_created_at
-    assert_equal Time.parse("2012-03-27 14:54:13 UTC"), customers_list[13].created_at
-  end
-
-  def test_item_list_stores_updated_at
-    assert_equal Time.parse("2012-03-27 14:54:13 UTC"), customers_list[13].updated_at
+  def test_customer_list_stores_customers
+    assert_equal Customer, customers_list.first.class
   end
 
   def test_all_method_returns_all_transactions
@@ -57,11 +41,4 @@ class CustomerRepositoryTest < Minitest::Test
     assert_equal 6, test_customer_repo.find_all_by_last_name("Kuhn")[0].id
   end
 
-  # def test_item_repo_calls_parent
-  #   parent = MiniTest::Mock.new
-  #   item_repo = ItemRepository.new("./data/items_one.csv", parent)
-  #   parent.expect(:find_merchant_by_merchant_id, nil, [26])
-  #   item_repo.find_merchant_by_merchant_id(26)
-  #   parent.verify
-  # end
 end
